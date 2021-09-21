3c72be4095c4517859bb4a71dd6df059d8d0e026
caburgoss <caburgoss@gmail.com>
2021-09-20 22:23:51 -0500

    Update DAOArtistas.java

--- 4. Desarrollo/S28-Grupo8-Rockola/src/main/java/dao/DAOArtistas.java
+++ 4. Desarrollo/S28-Grupo8-Rockola/src/main/java/dao/DAOArtistas.java
@@ -5,10 +5,57 @@
  */
 package dao;
 
+import db.ConexionDB;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import to.TOArtistas;
+
 /**
  *
  * @author Carlos
  */
 public class DAOArtistas {
+    private final ConexionDB con = new ConexionDB();
+    private String nombreTabla = "artistas";
+    private String [] columnas = {"idArtistas","nombreArtistas","nacionalidadArtistas"};
+
+    public DAOArtistas() {
+    }
+    
+    public int insertarAlbum(TOArtistas ToArtistas){
+        String [] valores = {String.valueOf(ToArtistas.getIdArtistas()),ToArtistas.getNombreArtistas(),ToArtistas.getNacionalidadArtistas()};
+        return con.insertar(nombreTabla, columnas, valores);
+    }
+    
+    public boolean actualizarAlbum(TOArtistas ToArtistas){
+        String [] valores = {String.valueOf(ToArtistas.getIdArtistas()),ToArtistas.getNombreArtistas(),ToArtistas.getNacionalidadArtistas()};
+        return con.actualizar(nombreTabla, columnas, valores, ToArtistas.getIdArtistas());
+    }
+    
+    public boolean eliminarArtistas(TOArtistas ToArtistas){
+        return con.borrar(nombreTabla,ToArtistas.getIdArtistas());
+    }
+    
+    public ArrayList<TOArtistas> listarArtistas(){
+        ArrayList<TOArtistas> artistas = new ArrayList<>();
+        TOArtistas artista;
+        try {
+            ResultSet rs = con.consultarTabla(nombreTabla);
+            while(rs.next()){
+                artista = new TOArtistas();
+                artista.setIdArtistas(rs.getInt("idArtistas"));
+                artista.setNombreArtistas(rs.getString("nombreArtistas"));
+                artista.setNacionalidadArtistas("nacionalidadArtistas");
+                artistas.add(artista);
+            }
+            return artistas;
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOAlbum.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
     
 }
